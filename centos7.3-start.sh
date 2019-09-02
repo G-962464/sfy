2635a7dc98912f3c5bd05bf723bd5de099ab40bc
G-962464 <53587964+G-962464@users.noreply.github.com>
2019-09-02 11:40:00 +0800

    Update centos7.3-start.sh
    加一个安装netstat命令

--- centos7.3-start.sh
+++ centos7.3-start.sh
@@ -1,1 +1,1 @@
-systemctl stop firewalld&&systemctl disable firewalld&&yum -y install lrzsz&&yum -y install vim&&echo 'Congratuations!'.
+systemctl stop firewalld&&systemctl disable firewalld&&yum -y install lrzsz&&yum -y install vim&&yum -y install net-tools&&echo 'Congratuations!'.
